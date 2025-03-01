6a985b0e8601de5bfeb23a29932d8348d3204a8a
Enaya Jawed <jawedenaya@gmail.com>
2025-02-28 22:26:10 -0600

    Added answer choice cards

--- db/queries.ts
+++ db/queries.ts
@@ -29,7 +29,7 @@ export const getUnits = cache(async () => {
     return [];
   }
 
-  // TODO: confirm whether order is needed
+
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
