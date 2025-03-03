5fe14918c795073a23408d5b3334694986bd475c
Enaya Jawed <jawedenaya@gmail.com>
2025-03-02 18:15:53 -0600

    Added Ryan's admin auth

--- lib/admin.ts
+++ lib/admin.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 
 const adminIds = [
   "user_2spL6BR9N3NP6fVA5wwI92XYPxA",
+  "user_2tmZQJblZ06JaRM4Ni7eQx9WYht",
+  "user_2tmWuyFwDXJxKa5TyIrGwV3rTn3",
 ]
 
 export const getIsAdmin = async () => {
