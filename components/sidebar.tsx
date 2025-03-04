d343f0760e47218fa1833c4b2950db49e8fbbb0c
Enaya Jawed <jawedenaya@gmail.com>
2025-03-04 08:52:23 -0600

    Minor changes to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const Sidebar = ({className}:Props) => {
   return(
-    <div className={cn("flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col", className,)}>
+    <div className={cn("flex h-full lg:w-[290px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col", className,)}>
       <Link href="/learn">
       <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
             <Image src="/tempLogo.png" height={40} width={40} alt="Mascot"/>
