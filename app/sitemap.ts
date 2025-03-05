bb5971be2bfe4ebf1be050766b0d1bef73f05fbd
Enaya Jawed <jawedenaya@gmail.com>
2025-03-05 10:31:28 -0600

    Removed authentication required URLs from sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,22 +6,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://codeepedia.com/',
       changeFrequency: 'yearly',
       priority: 1,
-    },
-    {
-      url: 'https://codeepedia.com/courses',
-      priority: 0.9,
-    },
-    {
-      url:'https://codeepedia.com/learn',
-      priority: 0.5,
-    },
-    {
-      url:'https://codeepedia.com/lesson',
-      priority: 0.4,
-    },
-    {
-      url:'https://codeepedia.com/quests',
-      priority:0.3,
     }
   ]
 }
